f778cb557ea4494483460aaca43cf29f5ff80e31
rahullohchab5210 <rahullohchab5210@gmail.com>
2025-11-18 12:38:56 +0530

    icon import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logoImg from "../assets/images/monkey-logo.png"
-import { SOCIAL_ICONS } from "../icon";
 import Button from "./button";
+import { SOCIAL_ICONS } from "./icon";
 
 function Navbar(props) {
     const [menuOpen, setMenuOpen] = useState(null);
